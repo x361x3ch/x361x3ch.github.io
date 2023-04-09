d47bf4cc1a7310ab16a5ed45c1e6be75a684e3a1
x361x3ch <94523370+x361x3ch@users.noreply.github.com>
2023-04-09 22:17:20 +0300

    update script.js

--- kamod/script.js
+++ kamod/script.js
@@ -2,7 +2,4 @@ const themeToggle = document.querySelector('#theme-toggle');
 
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
-  themeToggle.classList.toggle('dark-mode');
-  const themeToggleCircle = document.querySelector('#theme-toggle-circle');
-  themeToggleCircle.style.left = themeToggleCircle.style.left === '2.5px' ? '32.5px' : '2.5px';
 });
