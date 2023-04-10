17dd64f08efccaaef503a05fa30333a8001665cd
x361x3ch <94523370+x361x3ch@users.noreply.github.com>
2023-04-10 22:04:22 +0300

    update script.js

--- kamod/script.js
+++ kamod/script.js
@@ -1,8 +1,12 @@
-const themeToggle = document.querySelector('#theme-toggle');
+const toggleSwitch = document.querySelector('.toggle-switch');
+const toggleCircle = document.querySelector('.toggle-circle');
 
-themeToggle.addEventListener('click', () => {
+toggleSwitch.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
-  themeToggle.classList.toggle('dark-mode');
-  const themeToggleCircle = document.querySelector('#theme-toggle-circle');
-  themeToggleCircle.style.left = themeToggleCircle.style.left === '2.5px' ? '32.5px' : '2.5px';
+  const isChecked = document.body.classList.contains('dark-mode');
+  if (isChecked) {
+    toggleCircle.style.transform = 'translateY(100%)';
+  } else {
+    toggleCircle.style.transform = 'translateY(0)';
+  }
 });
